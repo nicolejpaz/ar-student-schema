57540865531905518aae31ed8a729424fdc6d985
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-10-24 14:10:41 -0700

    Changed the way the many-to-many associations work.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,8 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-  has_and_belongs_to_many :teachers
+  # has_and_belongs_to_many :teachers
+  has_many :teachers, :through => :students_teachers
   validates :email, :uniqueness => true, :format => { :with => /\S@{1}\S*\S.{1}\S{2}/ }
   validates :age, :numericality => { :greater_than => 5 }
   validates :phone, :format => { :with => /\d{3}\W*\d{3}-{1}\d{4}/ }
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,17 +1,7 @@
 require_relative '../../db/config'
 
 class Teacher < ActiveRecord::Base
-  # validates :email, :uniqueness => true, :format => { :with => /\S@{1}\S*\S.{1}\S{2}/ }
-  # validates :age, :numericality => { :greater_than => 5 }
-  # validates :phone, :format => { :with => /\W\d{3}\W\s\d{3}-{1}\d{4}/ }
-  # def name
-  #   "#{first_name} #{last_name}"
-  # end
-
-  # def age
-  #   (DateTime.now - birthday).to_i / 365
-  # end
-  has_and_belongs_to_many :students
+  has_many :students, :through => :students_teachers
 
 
 end
