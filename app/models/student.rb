58378a1ecc1554528ad15f7612580be400db200b
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-10-23 20:32:52 -0700

    Validations... better submit a version now.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,16 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+  validates :email, :uniqueness => true, :format => { :with => /\S@{1}\S*\S.{1}\S{2}/ }
+  validates :age, :numericality => { :greater_than => 5 }
+  validates :phone, :format => { :with => /\W\d{3}\W\s\d{3}-{1}\d{4}/ }
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    (DateTime.now - birthday).to_i / 365
+  end
+
+  
 end
